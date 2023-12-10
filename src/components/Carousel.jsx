78d6000e2dca6f74c98222d7f9ef78ebb884ca3c
Nomboh Quentin <nombohq14@gmail.com>
2023-12-09 18:54:51 -0800

    carousel images uploaded

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,7 +5,7 @@ function Carousel() {
 		<div className="carousel section h-[300px] 800px:h-[400px] w-full">
 			<div id="slide1" className="carousel-item relative w-full">
 				<img
-					src="https://res.cloudinary.com/queentech/image/upload/v1699363811/Black_Yellow_Bold_Bag_Fashion_Sale_Banner_by0k8m.png"
+					src="https://res.cloudinary.com/queentech/image/upload/v1702176762/Black_Yellow_Bold_Bag_Fashion_Sale_Banner-min-min_yetral.jpg"
 					className="w-full"
 				/>
 				<div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -19,7 +19,7 @@ function Carousel() {
 			</div>
 			<div id="slide2" className="carousel-item relative w-full">
 				<img
-					src="https://res.cloudinary.com/queentech/image/upload/v1699363810/Gray_Red_Black_White_Dark_Red_Photo_Shopping_Black_Friday_Sale_Banner_rglo3f.png"
+					src="https://res.cloudinary.com/queentech/image/upload/v1702176762/Gray_Red_Black_White_Dark_Red_Photo_Shopping_Black_Friday_Sale_Banner-min-min_chy3dj.jpg"
 					className="w-full"
 				/>
 				<div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -33,7 +33,7 @@ function Carousel() {
 			</div>
 			<div id="slide3" className="carousel-item relative w-full">
 				<img
-					src="https://res.cloudinary.com/queentech/image/upload/v1699363808/Blue_Simple_Special_Offer_Big_Sale_Banner_Landscape_fdqwwq.png"
+					src="https://res.cloudinary.com/queentech/image/upload/v1702176763/Blue_Simple_Special_Offer_Big_Sale_Banner_Landscape-min-min_eetifn.jpg"
 					className="w-full"
 				/>
 				<div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
